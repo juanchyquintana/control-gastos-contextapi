cdb1786a1dea18d4b91733e43748365189d93301
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-05-20 11:59:41 -0300

    se agregó un hook y se hizo uso del context

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,18 +1,25 @@
-import { ChangeEvent, useMemo, useState } from "react";
+import { ChangeEvent, FormEvent, useMemo, useState } from "react";
+import { useBudget } from "../hooks/useBudget";
 
 export default function BudgetForm() {
   const [budget, setBudget] = useState(0);
+  const { state, dispatch } = useBudget();
 
-  const handleChange = (e : ChangeEvent<HTMLInputElement>) => {
-    setBudget(e.target.valueAsNumber)
-  }
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setBudget(e.target.valueAsNumber);
+  };
 
   const isValid = useMemo(() => {
-    return isNaN(budget) || budget <= 0
-  }, [budget])
+    return isNaN(budget) || budget <= 0;
+  }, [budget]);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch({ type: "add-budget", payload: { budget } });
+  };
 
   return (
-    <form className="space-y-5">
+    <form className="space-y-5" onSubmit={handleSubmit}>
       <div className="flex flex-col space-y-5">
         <label
           htmlFor="budget"
