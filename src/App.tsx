79233eb8ff929ad948de68d309475372fa7657a2
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-05-22 18:33:12 -0300

    se instalaron dependencias, se creó un helper y luego se crearon varios componentes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import BudgetForm from "./components/BudgetForm";
 import { useBudget } from "./hooks/useBudget";
 import BudgetTracker from "./components/BudgetTracker";
 import ExpenseModal from "./components/ExpenseModal";
+import ExpenseList from "./components/ExpenseList";
 
 function App() {
   const { state } = useBudget();
@@ -22,7 +23,9 @@ function App() {
       </div>
 
       {isValidBudget && (
-        <main className="max-w-3xl max-auto py-10">
+        <main className="max-w-3xl mx-auto py-10">
+          <ExpenseList />
+
           <ExpenseModal />
         </main>
       )}
--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,9 +1,11 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { categories } from "../data/categories";
 import type { DraftExpense, Value } from "../types";
 import DatePicker from "react-date-picker";
 import "react-calendar/dist/Calendar.css";
 import "react-date-picker/dist/DatePicker.css";
+import ErrorMessage from "./ErrorMessage";
+import { useBudget } from "../hooks/useBudget";
 
 export default function ExpenseForm() {
   const [expense, setExpense] = useState<DraftExpense>({
@@ -12,6 +14,8 @@ export default function ExpenseForm() {
     category: "",
     date: new Date(),
   });
+  const [error, setError] = useState('')
+  const { dispatch } = useBudget()
 
   const handleChangeDate = (value: Value) => {
     setExpense({
@@ -29,11 +33,31 @@ export default function ExpenseForm() {
     })
   }
 
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if(Object.values(expense).includes('')) {
+      setError('Todos los campos son obligatorios')
+      return
+    }
+
+    dispatch({ type: "add-expense", payload: { expense }})
+
+    setExpense({
+      amount: 0,
+      expenseName: "",
+      category: "",
+      date: new Date(),
+    })
+  }
+
   return (
-    <form className="space-y-5">
+    <form className="space-y-5" onSubmit={handleSubmit}>
       <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
         Nuevo Gasto
       </legend>
+
+      {error && <ErrorMessage>{error}</ErrorMessage>}
 
       <div className="flex flex-col gap-2">
         <label htmlFor="expenseName" className="text-xl">
